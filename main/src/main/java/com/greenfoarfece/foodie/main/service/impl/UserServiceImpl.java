b49f1b37bedf81b56c0ed83d8ba069147169db17
sayid.sidqi <sayid.sidqi@avivaid.local>
2017-04-21 16:11:35 +0700

    Fixed spring annotation used.

--- main/src/main/java/com/greenfoarfece/foodie/main/service/impl/UserServiceImpl.java
+++ main/src/main/java/com/greenfoarfece/foodie/main/service/impl/UserServiceImpl.java
@@ -1,13 +1,13 @@
 package com.greenfoarfece.foodie.main.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.greenfoarfece.foodie.main.dao.UserDao;
 import com.greenfoarfece.foodie.main.entity.User;
 import com.greenfoarfece.foodie.main.service.UserService;
 
-@Repository("userService")
+@Service("userService")
 public class UserServiceImpl implements UserService {
 	
 	@Autowired
