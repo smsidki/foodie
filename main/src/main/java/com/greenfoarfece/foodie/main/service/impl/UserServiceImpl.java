e6d80ee693308a108705758a3a1e319e1db9bcfd
sayid.sidqi <sayid.sidqi@avivaid.local>
2017-05-15 15:17:35 +0700

    Remove unnecessary comments

--- main/src/main/java/com/greenfoarfece/foodie/main/service/impl/UserServiceImpl.java
+++ main/src/main/java/com/greenfoarfece/foodie/main/service/impl/UserServiceImpl.java
@@ -46,56 +46,5 @@ public class UserServiceImpl implements UserService {
 	public boolean deletePermanently(User user) {
 		return userDao.deletePermanently(user);
 	}
-
-	/*
-	@Override
-	public User getUser() {
-		return userDao.find(Long.valueOf(1));
-	}
-	
-	@Override
-	public User getUserIncludeDeleted() {
-		return userDao.findIncludeDeleted(Long.valueOf(1));
-	}
-
-	@Override
-	public User insert() {
-		return userDao.save(generateUser());
-	}
-
-	@Override
-	public boolean delete() {
-		return userDao.delete(Long.valueOf(1));
-	}
-	
-	@Override
-	public boolean deletePermanently() {
-		return userDao.deletePermanently(getUserIncludeDeleted());
-	}
-
-	private User generateUser() {
-		Date date = new Date();
-		User userAll = new User(Long.valueOf(0), "Sidqi", "Sidqi", date, date, Status.ACTIVE, "koko", "ayayawae", Role.CUSTOMER);
-		User userNo = new User();
-		userNo.setCreatedBy("Sidqi");
-		userNo.setCreatedOn(date);
-		userNo.setLastUpdatedBy("Sidqi");
-		userNo.setLastUpdatedOn(date);
-		userNo.setPassword("ayoayo");
-		userNo.setRole(Role.ADMIN);
-		userNo.setStatus(Status.ACTIVE);
-		userNo.setUserName("Ohaiyo");
-		return User.builder()
-				   .createdBy("Sidqi")
-				   .createdOn(date)
-				   .lastUpdatedBy("Sidqi")
-				   .lastUpdatedOn(date)
-				   .password("Ohiya")
-				   .role(Role.MERCHANT)
-				   .status(Status.ACTIVE)
-				   .userName("Dahen")
-				   .build();
-	}
-	*/
 	
 }
